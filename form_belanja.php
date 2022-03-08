026ed9b7bf0c581a6c6b970842536d2ad78d133f
Shofink <91359694+Shofink@users.noreply.github.com>
2022-03-08 19:58:05 +0700

    Add files via upload

---
+++ form_belanja.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"> 
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+<form action="form_belanja.php" method="POST">
+  <div class="form-group row">
+    <label for="customer" class="col-4 col-form-label">Customer</label> 
+    <div class="col-8">
+      <input id="customer" name="customer" placeholder="Nama Customer" type="text" class="form-control">
+    </div>
+  </div>
+  <div class="form-group row">
+    <label class="col-4">Pilih produk</label> 
+    <div class="col-8">
+      <div class="custom-control custom-radio custom-control-inline">
+        <input name="produk" id="produk_0" type="radio" class="custom-control-input" value="TV"> 
+        <label for="produk_0" class="custom-control-label">TV</label>
+      </div>
+      <div class="custom-control custom-radio custom-control-inline">
+        <input name="produk" id="produk_1" type="radio" class="custom-control-input" value="Kulkas"> 
+        <label for="produk_1" class="custom-control-label">Kulkas</label>
+      </div>
+      <div class="custom-control custom-radio custom-control-inline">
+        <input name="produk" id="produk_2" type="radio" class="custom-control-input" value="Mesin Cuci"> 
+        <label for="produk_2" class="custom-control-label">Mesin Cuci</label>
+      </div>
+    </div>
+  </div>
+  <div class="form-group row">
+    <label for="jumlah" class="col-4 col-form-label">Jumlah</label> 
+    <div class="col-8">
+      <input id="jumlah" name="jumlah" placeholder="Jumlah" type="text" class="form-control">
+    </div>
+  </div>
+  <div class="form-group row">
+    <div class="offset-4 col-8">
+      <button name="proses" type="submit" class="btn btn-primary">Kirim</button>
+    </div>
+  </div>
+</form>
+<?php
+
+$proses = $_POST["proses"];
+$customer = $_POST["customer"];
+$produk = $_POST['produk']
+$jumlah = $_POST["jumlah"];
+$tv = 4200000;
+$kulkas = 3100000;
+$mesincuci = 3800000;
+
+echo '<hr>';
+echo 'Nama Customer : ' .$customer;
+echo '<br> Produk Pilihan : ' .$produk;
+echo '<br> Jumlah Beli: ' .$jumlah;
+echo '<br> Total Belanja : ' .$total;
+
+if
+if ($produk1 == $tv){
+   echo $harga = 4200000 * $jumlah;
+} elseif ($produk == $kulkas){
+    echo $harga = 3100000 * $jumlah;
+}  else {
+    echo $harga = 3800000 * $jumlah;
+}
+
+
+?>
+</body>
+</html>
